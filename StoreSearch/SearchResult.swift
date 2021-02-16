e12fde9a09c0f10f858065990ebd549d1e30cd2a
Stefan Minchevski <stefan.minchevski@yahoo.com>
2021-02-16 16:30:03 +0100

    Add a search bar

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Stefan Minchevski on 16.2.21.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
